7faf1d5a0c203ec8aa99933ee688f54f2a1ba00e
icedwater <icedwater@gmail.com>
2024-11-21 11:06:17 +0800

    Updated docstrings and rearranged imports.

--- cal_mean_variance.py
+++ cal_mean_variance.py
@@ -1,8 +1,13 @@
-import numpy as np
-import os
-from os.path import join as pjoin
+"""
+Calculate the mean and variance of sequence vectors for a given rig type.
 
-
+# each frame is described in four broad sections given a rig with j joints:
+# 0 -- 3                                : global position, 4
+# 4 -- 4 + (j - 1) * 3                  : local positions of other joints with respect to root, (j - 1) * 3
+# 4 + (j - 1) * 3 -- 4 + (j - 1) * 9    : local rotations of other joints with respect to root, (j - 1) * 6
+# 4 + (j - 1) * 9 -- end                : global joint velocities plus foot contact information, j * 3 + 4
+# see https://arxiv.org/pdf/2405.11126 Appendix A for more details
+# from the computed data, this corresponds to:
 # root_rot_velocity (B, seq_len, 1)
 # root_linear_velocity (B, seq_len, 2)
 # root_y (B, seq_len, 1)
@@ -10,7 +15,21 @@ from os.path import join as pjoin
 # rot_data (B, seq_len, (joint_num - 1)*6)
 # local_velocity (B, seq_len, joint_num*3)
 # foot contact (B, seq_len, 4)
-def mean_variance(data_dir, save_dir, joints_num):
+
+"""
+import os
+from os.path import join as pjoin
+import numpy as np
+
+
+def mean_variance(data_dir: str, save_dir: str, joints_num: int):
+    """
+    Compute the mean and variance of the joint vector sequences in a given directory.
+
+    :param data_dir:    string path to the vectors whose mean and variance will be computed
+    :param save_dir:    string path to the directory where the results will be stored
+    :param joints_num:  integer number of joints for a given rig type
+    """
     file_list = os.listdir(data_dir)
     data_list = []
 
@@ -23,12 +42,6 @@ def mean_variance(data_dir, save_dir, joints_num):
 
     data = np.concatenate(data_list, axis=0)
     print(data.shape)
-    # each frame is described in four broad sections given a rig with j joints: 
-    # 0 -- 3                                : global position, 4
-    # 4 -- 4 + (j - 1) * 3                  : local positions of other joints with respect to root, (j-1) * 3
-    # 4 + (j - 1) * 3 -- 4 + (j - 1) * 9    : local rotations of other joints with respect to root, (j-1) * 6
-    # 4 + (j - 1) * 9 -- end                : global joint velocities plus foot contact information, j * 3 + 4 
-    # see https://arxiv.org/pdf/2405.11126 Appendix A for more details
     section_one = 4 + (joints_num - 1) * 3
     section_two = 4 + (joints_num - 1) * 9
     section_three = section_two + joints_num * 3
